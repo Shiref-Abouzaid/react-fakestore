a78966d4e5c9841dac91202b92eb74d91cedd121
Sherif <s.abouzaid@awaed.co>
2023-10-04 10:57:44 +0200

    remove useless files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Welcome from "./components/Welcome";
 import React, { useState, useEffect } from "react";
 import Product from "./components/Product";
 import Navbar from "./components/Navbar";
